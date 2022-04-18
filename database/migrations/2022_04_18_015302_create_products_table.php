87f6ceb0dcd9a8069eeccc3d52f23d047c2e6b49
rafkimustafa <rafkimustafa@gmail.com>
2022-04-18 15:18:05 +0700

    menambah field pada tabel product.

--- database/migrations/2022_04_18_015302_create_products_table.php
+++ database/migrations/2022_04_18_015302_create_products_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id')->index();
+            $table->string('name');
+            $table->float('price',8 , 2);
+            $table->boolean('status');
             $table->timestamps();
         });
     }
